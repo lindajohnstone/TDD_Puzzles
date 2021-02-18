d7ff0e6741cb58362bd5557967c4992e214bc57d
Linda Johnstone <linda.johnstone@myob.com>
2021-02-18 14:52:07 +1100

    Initial commit

---
+++ TDD_Puzzles.Tests/TDD_PuzzlesTests.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace TDD_Puzzles.Tests
+{
+    public class TDD_PuzzlesTests
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ TDD_Puzzles/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TDD_Puzzles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
