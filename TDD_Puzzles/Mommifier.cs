dba7c6d82d9f95923793d8b917f65114046a66f6
Linda Johnstone <linda.johnstone@myob.com>
2021-02-18 15:27:39 +1100

    Removed redundant code

--- TDD_Puzzles/Mommifier.cs
+++ TDD_Puzzles/Mommifier.cs
@@ -6,8 +6,7 @@ namespace TDD_Puzzles.Tests
     {
         public static string WriteString(string word)
         {
-            word = word.Replace("i", "mommy");
-            return word;
+            return word.Replace("i", "mommy");
         }
     }
 }
